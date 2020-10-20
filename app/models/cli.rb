12226a3b49f10940608823871340f62c286b9158
wlytle <wlytle89@gmail.com>
2020-10-20 15:23:19 -0700

    adds CLI class

---
+++ app/models/cli.rb
@@ -0,0 +1,18 @@
+class CLI
+  def initialize
+    puts "Welcome to Politicians and money and stuff"
+    puts "=" * 45
+  end
+
+  def main_menu
+    puts "Main Menu"
+    puts "-" * 30
+    puts "1. Search for congress members by location"
+    puts "2. Find congress members by name"
+    puts "3. Search legislation by issue"
+    puts "4. Search legislation by bill name"
+    puts "5. Exit"
+    puts
+    puts "Enter your choice:"
+  end
+end
