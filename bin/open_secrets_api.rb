dbc9da9e13772a73b58d02644c31222fc401b523
wlytle <wlytle89@gmail.com>
2020-10-21 17:10:12 -0700

    merges open secrets and generates industry and donations classes

--- bin/open_secrets_api.rb
+++ bin/open_secrets_api.rb
@@ -2,26 +2,26 @@ require "faraday"
 require "json"
 require "pry"
 
-base = "http://www.opensecrets.org/api"
-key = "40dca1f69a36b95c944aad4672acbc2b"
-options = {
-  :apikey => key,
-  :output => "json",
-  :method => "candIndustry",
-  #:id => "TN",
-  #:id => "D000031292",
-  :cid => "N00007360",
-#q:cycle => "2018",
-#:org => "oil",
-}
-header = {
-  "X-API-Key" => key,
-  "Accept" => "application/json",
-}
-response = Faraday.get(base, options, header)
+# base = "http://www.opensecrets.org/api"
+# key = "40dca1f69a36b95c944aad4672acbc2b"
+# options = {
+#   :apikey => key,
+#   :output => "json",
+#   :method => "candIndustry",
+#   #:id => "TN",
+#   #:id => "D000031292",
+#   :cid => "N00007360",
+# #q:cycle => "2018",
+# #:org => "oil",
+# }
+# header = {
+#   "X-API-Key" => key,
+#   "Accept" => "application/json",
+# }
+# response = Faraday.get(base, options, header)
 
-binding.pry
-0
+# binding.pry
+# 0
 
 #Get infos
 def query_for_finances
@@ -32,18 +32,20 @@ def query_for_finances
     "Accept" => "application/json",
   }
   CongressMember.all.each do |cm|
-    options = {
-      :apikey => key,
-      :output => "json",
-      :method => "candIndustry",
-      :cid => cm.crp_id,
-    }
-    response = JSON.parse(Faraday.get(base, options, header).body)
-    response["response"]["industries"]["industry"].each do |ind|
-      industry_name = ind["@attributes"]["industry_name"]
-      total = ["@attributes"]["total"]
-      industry = Industry.find_or_create_by(:name => industry_name)
-      Donation.create(congress_member: cm, amount: total, industry: industry)
+    if cm.crp_id
+      options = {
+        :apikey => key,
+        :output => "json",
+        :method => "candIndustry",
+        :cid => cm.crp_id,
+      }
+      response = JSON.parse(Faraday.get(base, options, header).body)
+      response["response"]["industries"]["industry"].each do |ind|
+        industry_name = ind["@attributes"]["industry_name"]
+        total = ind["@attributes"]["total"]
+        industry = Industry.find_or_create_by(:name => industry_name)
+        Donation.create(congress_member: cm, amount: total, industry: industry)
+      end
     end
   end
 end
--- bin/runapi.rb
+++ bin/runapi.rb
@@ -1,19 +1,10 @@
-require_relative '../config/environment'
-require_relative 'api.rb'
+require_relative "../config/environment"
+require_relative "api.rb"
+require_relative "open_secrets_api"
 
-# industry_array = ["tobacco", "pharmaceuticals", "climate", "oil", "healthcare"]
+#industry_array = ["Retired", "Health Professionals", "Education", "Lawyers/Law Firms", "Hospitals / Nursing Homes", "Securities & Investment", "Electronics MFG & Equip", "Public Sector Unions", "Misc Finance"]
 
-industry_array = ["Retired", "Health Professionals", "Education", "Lawyers/Law Firms", "Hospitals / Nursing Homes", "Securities & Investment", "Electronics MFG & Equip", "Public Sector Unions", "Misc Finance"]
-
-create_industries(industry_array)
-add_all_bills_for_all_industries(Industry.all)
+#create_industries(industry_array)
+#add_all_bills_for_all_industries(Industry.all)
 create_congress_members
-
-# def start
-#   puts "Enter an industry"
-#   input = gets.chomp.downcase
-#   create_bills_by_industry(input)
-# end
-
-#start
-
+query_for_finances
